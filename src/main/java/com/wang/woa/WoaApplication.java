6794b83bba5d9547ea00fd37cc1d6c0bafa7f393
wangzikang <wangzk@hellotax.cn>
2022-07-27 21:07:05 +0800

    初始化仓库

---
+++ src/main/java/com/wang/woa/WoaApplication.java
@@ -0,0 +1,13 @@
+package com.wang.woa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wang/woa/WoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.woa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
